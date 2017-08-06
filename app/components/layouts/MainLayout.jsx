3d5bff7b3e20318f01c56aca9eed5e966822b057
Thanapat Pirmphol <oadtz@Thanapats-MacBook-Pro.local>
2017-08-06 17:06:59 +0700

    Scroll to top when change route

--- app/components/layouts/MainLayout.jsx
+++ app/components/layouts/MainLayout.jsx
@@ -4,6 +4,10 @@ import SiteNav from 'SiteNav';
 import SiteFooter from 'SiteFooter';
 
 class MainLayout extends Component {
+    componentDidMount () {
+        window.scrollTo(0, 0);
+    }
+
     render () {
         const {children} = this.props;
 
---
+++ app/tests/api/postAPI.test.jsx
@@ -0,0 +1,11 @@
+import expect from 'expect';
+import moment from 'moment';
+
+import postAPI from 'postAPI';
+
+describe('postAPI', () => {
+    it('should exist', () => {
+        expect(postAPI).toExist();
+    });
+
+});
